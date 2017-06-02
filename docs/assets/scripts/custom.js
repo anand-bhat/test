c1fadb8c8e2ad5e79386ebe829a7ddd7106d4e33
Anand Bhat <anand-bhat@users.noreply.github.com>
2017-06-02 19:34:49 +0530

    T

--- docs/assets/scripts/custom.js
+++ docs/assets/scripts/custom.js
@@ -234,8 +234,7 @@ $(document).ready(function () {
 			{type: 'enumgrade', targets: 4},
 			{visible: false, targets: [0, 5, 6, 7, 8, 9, 10]}
 		],
-		//data: dataSet,
-		ajax: '../assets/data/dataSetIndianPaymentServices.json',
+		data: dataSet,
 		deferRender: true,
 		fixedHeader: { footer: true, header: true },
 		initComplete: function () {
