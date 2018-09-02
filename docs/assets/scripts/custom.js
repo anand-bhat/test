efd5dc448e83b0ccd3a761c7d61d80af31d060c7
Anand Bhat <anand-bhat@users.noreply.github.com>
2018-09-03 08:22:33 +1000

    T

--- docs/assets/scripts/custom.js
+++ docs/assets/scripts/custom.js
@@ -294,7 +294,7 @@ $(document).ready(function () {
 					var milestone = row[7];
 
 					var details = '?title=' + encodeURIComponent(title) + '&body=' + encodeURIComponent(body) + labelsText + '&milestone=' + encodeURIComponent(milestone);
-					return '<a href="https://github.com/anand-bhat/httpswatch/issues/new' + details + '" rel="noopener" target="_blank">Create</a>';
+					return '<a href="https://github.com/anand-bhat/httpswatch/issues/new' + details + '" rel="noopener nofollow" target="_blank">Create</a>';
 				},
 				targets: 10
 			},
